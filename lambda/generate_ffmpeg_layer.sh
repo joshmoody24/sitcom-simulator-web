0fce70d55eba4cb39569c986b92a62d34cab3b79
Josh Moody <joshmoody24@gmail.com>
2024-01-10 00:51:06 -0700

    added ffmpeg layer generator

---
+++ lambda/generate_ffmpeg_layer.sh
@@ -0,0 +1,15 @@
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz.md5
+
+md5sum -c ffmpeg-release-amd64-static.tar.xz.md5
+
+tar xvf ffmpeg-release-amd64-static.tar.xz
+
+mkdir -p ffmpeg/bin
+
+cp ffmpeg-4.3.1-amd64-static/ffmpeg ffmpeg/bin/
+
+cd ffmpeg
+
+zip -r ../ffmpeg.zip .
